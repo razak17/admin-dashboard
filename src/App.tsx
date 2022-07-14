76d1366c0bf7abe7df421926d9df63efbab274de
Razak <moabdulrazak17@gmail.com>
2022-07-14 20:13:11 +0000

    feat: create settings tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1 className='underline text-3xl'>Hello Mom!</h1>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content="Settings"
+              position="TopRight"
+            >
+              <button
+                type="button"
+                style={{ background: 'blue', borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
