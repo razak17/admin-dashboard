19aec9a94831d9ea17a7b1d8e3d94cd067885be3
Razak <moabdulrazak17@gmail.com>
2022-07-25 13:49:49 +0000

    chore: setup formatting and linting with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -5,98 +5,98 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {
-  Ecommerce,
-  Orders,
-  Calendar,
-  Employees,
-  Stacked,
-  Pyramid,
-  Customers,
-  Kanban,
-  Line,
-  Area,
-  Bar,
-  PieChart,
-  Financial,
-  ColorPicker,
-  ColorMapping,
-  Editor
+	Ecommerce,
+	Orders,
+	Calendar,
+	Employees,
+	Stacked,
+	Pyramid,
+	Customers,
+	Kanban,
+	Line,
+	Area,
+	Bar,
+	PieChart,
+	Financial,
+	ColorPicker,
+	ColorMapping,
+	Editor
 } from './pages';
 import { useStateContext } from './context/ContextProvider';
 
 import './App.css';
 
 const App: React.FC = () => {
-  const { activeMenu } = useStateContext();
+	const { activeMenu } = useStateContext();
 
-  return (
-    <div>
-      <BrowserRouter>
-        <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
-            <TooltipComponent content='Settings' position='TopRight'>
-              <button
-                type='button'
-                style={{ background: 'blue', borderRadius: '50%' }}
-                className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-          </div>
-          {activeMenu ? (
-            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-              <Sidebar />
-            </div>
-          ) : (
-            <div className='w-0 dark:bg-secondary-dark-bg'>
-              <Sidebar />
-            </div>
-          )}
-          <div
-            className={
-              activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
-            }
-          >
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-              <Navbar />
-            </div>
-            <div>
-              <Routes>
-                {/* dashboard  */}
-                <Route path='/' element={<Ecommerce />} />
-                <Route path='/ecommerce' element={<Ecommerce />} />
+	return (
+		<div>
+			<BrowserRouter>
+				<div className='flex relative dark:bg-main-dark-bg'>
+					<div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+						<TooltipComponent content='Settings' position='TopRight'>
+							<button
+								type='button'
+								style={{ background: 'blue', borderRadius: '50%' }}
+								className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
+							>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+					{activeMenu ? (
+						<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+							<Sidebar />
+						</div>
+					) : (
+						<div className='w-0 dark:bg-secondary-dark-bg'>
+							<Sidebar />
+						</div>
+					)}
+					<div
+						className={
+							activeMenu
+								? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+								: 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
+						}
+					>
+						<div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+							<Navbar />
+						</div>
+						<div>
+							<Routes>
+								{/* dashboard  */}
+								<Route path='/' element={<Ecommerce />} />
+								<Route path='/ecommerce' element={<Ecommerce />} />
 
-                {/* pages  */}
-                <Route path='/orders' element={<Orders />} />
-                <Route path='/employees' element={<Employees />} />
-                <Route path='/customers' element={<Customers />} />
+								{/* pages  */}
+								<Route path='/orders' element={<Orders />} />
+								<Route path='/employees' element={<Employees />} />
+								<Route path='/customers' element={<Customers />} />
 
-                {/* apps  */}
-                <Route path='/kanban' element={<Kanban />} />
-                <Route path='/editor' element={<Editor />} />
-                <Route path='/calendar' element={<Calendar />} />
-                <Route path='/color-picker' element={<ColorPicker />} />
+								{/* apps  */}
+								<Route path='/kanban' element={<Kanban />} />
+								<Route path='/editor' element={<Editor />} />
+								<Route path='/calendar' element={<Calendar />} />
+								<Route path='/color-picker' element={<ColorPicker />} />
 
-                {/* charts  */}
-                <Route path='/line' element={<Line />} />
-                <Route path='/area' element={<Area />} />
-                <Route path='/bar' element={<Bar />} />
-                <Route path='/pie' element={<PieChart />} />
-                <Route path='/financial' element={<Financial />} />
-                <Route path='/color-mapping' element={<ColorMapping />} />
-                <Route path='/pyramid' element={<Pyramid />} />
-                <Route path='/stacked' element={<Stacked />} />
-              </Routes>
-            </div>
-            <Footer />
-          </div>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+								{/* charts  */}
+								<Route path='/line' element={<Line />} />
+								<Route path='/area' element={<Area />} />
+								<Route path='/bar' element={<Bar />} />
+								<Route path='/pie' element={<PieChart />} />
+								<Route path='/financial' element={<Financial />} />
+								<Route path='/color-mapping' element={<ColorMapping />} />
+								<Route path='/pyramid' element={<Pyramid />} />
+								<Route path='/stacked' element={<Stacked />} />
+							</Routes>
+						</div>
+						<Footer />
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
