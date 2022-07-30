dfd97567ef15a12e06c49a94f6a37c8c03716f84
Razak <moabdulrazak17@gmail.com>
2022-07-30 17:18:33 +0000

    chore(app): implement calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+  View
+} from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
-const Calendar = () => {
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
+const PropertyPane: React.FC<{ children: React.ReactNode }> = props => (
+  <div className='mt-5'>{props.children}</div>
+);
+
+const Scheduler = () => {
+  const [scheduleObj, setScheduleObj] = useState<ScheduleComponent>();
+
+  const change = (args: { value: Date }) => {
+    if (scheduleObj) {
+      scheduleObj.selectedDate = args.value;
+      scheduleObj.dataBind();
+    }
+  };
+
+  const onDragStart = (arg: { navigation: { enable: boolean } }) => {
+    arg.navigation.enable = true;
+  };
+
   return (
-    <div>Calendar</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Calendar' />
+      <ScheduleComponent
+        height='650px'
+        ref={schedule => setScheduleObj(schedule as ScheduleComponent)}
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        dragStart={onDragStart}
+      >
+        <ViewsDirective>
+          {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map(item => (
+            <ViewDirective key={item} option={item as View} />
+          ))}
+        </ViewsDirective>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+      <PropertyPane>
+        <table style={{ width: '100%', background: 'white' }}>
+          <tbody>
+            <tr style={{ height: '50px' }}>
+              <td style={{ width: '100%' }}>
+                <DatePickerComponent
+                  value={new Date(2021, 0, 10)}
+                  showClearButton={false}
+                  placeholder='Current Date'
+                  floatLabelType='Always'
+                  change={change}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </PropertyPane>
+    </div>
   );
 };
 
-export default Calendar;
+export default Scheduler;
